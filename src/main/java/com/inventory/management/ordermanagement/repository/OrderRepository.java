626dd83b978848228c788fc314f8f4473c714af1
It25101126 <IT25101126@my.sliit.lk>
2026-05-16 10:54:19 +0530

    Created OrderRepository, PurchaseOrderRepository & ReturnOrderRepository

---
+++ src/main/java/com/inventory/management/ordermanagement/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.inventory.management.ordermanagement.repository;
+
+
+import com.inventory.management.ordermanagement.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByStatus(String status);
+
+    List<Order> findByStatusNot(String status);
+}
